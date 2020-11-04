de5fd3fcd0751eaad60fe5b30272ef493fca129e
meet <1194735712@qq.com>
2020-11-04 10:25:49 +0800

    ccc

---
+++ app/build/generated/source/buildConfig/debug/com/cx/meet/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cx.meet;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cx.meet";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final boolean LOG_DEBUG = true;
+  public static final String LOG_TAG = "Meet";
+  public static final String SP_NAME = "Config";
+}
